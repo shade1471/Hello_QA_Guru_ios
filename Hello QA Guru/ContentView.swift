14c103918c85c78bce7913c75cc84092b9789cb3
Andrey Gribanov <ag@MacAir-Sequoia.local>
2025-05-08 00:16:58 +0700

    initial commit

---
+++ Hello QA Guru/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hello QA Guru
+//
+//  Created by Andrey Gribanov on 07.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, QA Guru!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hello QA Guru/Hello_QA_GuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_QA_GuruApp.swift
+//  Hello QA Guru
+//
+//  Created by Andrey Gribanov on 07.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Hello_QA_GuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
